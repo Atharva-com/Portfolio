98cea9553f47eb16c42b4bb843ec7b16ac5a0275
Atharva Barve <121193386+Atharva-com@users.noreply.github.com>
2023-07-15 22:11:28 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,13 +7,6 @@ import { usePathname } from 'next/navigation'
 import { useEffect, useState } from 'react'
 import Transition from '@/components/Transition'
 
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
-  title: 'Atharva.Portfolio',
-  description: 'A Fine Description of Atharva',
-}
-
 export default function RootLayout({ children }) {
   const pathname = usePathname()
   const [url, seturl] = useState(`${pathname}`)
@@ -22,7 +15,7 @@ export default function RootLayout({ children }) {
   }, [pathname])
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Layout>
           <AnimatePresence mode='wait'>
 
